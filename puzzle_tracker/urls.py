8f2e803046cd696a35ed85ec7b165e59b7ad7b2e
curiousona <iamsonali45@gmail.com>
2025-07-16 15:21:50 +0530

    Update settings.py and urls.py

--- puzzle_tracker/urls.py
+++ puzzle_tracker/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('', lambda request: redirect('/puzzles/dashboard/')),
     path('admin/', admin.site.urls),
     path('puzzles/', include('puzzles.urls')),
-] 
+]
